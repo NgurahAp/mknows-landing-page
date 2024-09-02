4bcf2b053480e0287db6da8813a792bd69d3a6fb
Arya <ngurahpratama2002@gmail.com>
2024-09-02 16:45:23 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "custom-blue": "#D7E9F4",
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 export default config;

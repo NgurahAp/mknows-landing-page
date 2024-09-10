b8310e807724d50b2b234b5fb68a99aadce61af4
Arya <ngurahpratama2002@gmail.com>
2024-09-10 16:37:15 +0700

    fixin the card element

--- src/app/bootcamp/page.tsx
+++ src/app/bootcamp/page.tsx
@@ -39,30 +39,26 @@ export default function BootcampPage() {
         </div>
       </section>
       <section className="pb-16 pt-7 min-h-[100vh]">
-        
         <div className="container mx-auto">
-          <div className="flex flex-wrap justify-center ">
-            <div className="max-w-sm w-full md:mx-16 mx-14 my-3 h-[450px] border md:my-10  relative">
-              <Link href="">
-                <Image
-                  className="rounded-t-lg"
-                  src="/assets/bootcamp/frontend.png"
-                  alt=""
-                  width={500}
-                  height={300}
-                  layout="responsive"
-                />
-              </Link>
-              <div className="p-5">
-                <Link href="">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          <div className="flex flex-wrap justify-center">
+            {/* Card */}
+            <div className="max-w-sm bg-[#D8EFD3] w-full md:mx-8 mx-14 my-3 h-[450px] border md:my-10 relative flex flex-col">
+              <Image
+                className="rounded-t-lg"
+                src="/assets/bootcamp/frontend.png"
+                alt=""
+                width={500}
+                height={300}
+                layout="responsive"
+              />
+              <div className="p-5 -mt-20 flex flex-col flex-grow">
+                <div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight">
                     Test 1
                   </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-                  Description 1
-                </p>
-                <div className="flex justify-center">
+                  <p className="mb-3 font-normal">Description 1</p>
+                </div>
+                <div className="mt-auto flex justify-end">
                   <Link
                     href=""
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -95,27 +91,23 @@ export default function BootcampPage() {
                 />
               </div>
             </div>
-            <div className="max-w-sm w-full md:mx-16 mx-14 my-3 h-[450px] border md:my-10  relative">
-              <Link href="">
-                <Image
-                  className="rounded-t-lg"
-                  src="/assets/bootcamp/frontend.png"
-                  alt=""
-                  width={500}
-                  height={300}
-                  layout="responsive"
-                />
-              </Link>
-              <div className="p-5">
-                <Link href="">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            <div className="max-w-sm bg-[#D8EFD3] w-full md:mx-8 mx-14 my-3 h-[450px] border md:my-10 relative flex flex-col">
+              <Image
+                className="rounded-t-lg"
+                src="/assets/bootcamp/frontend.png"
+                alt=""
+                width={500}
+                height={300}
+                layout="responsive"
+              />
+              <div className="p-5 -mt-20 flex flex-col flex-grow">
+                <div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight">
                     Test 1
                   </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-                  Description 1
-                </p>
-                <div className="flex justify-center">
+                  <p className="mb-3 font-normal">Description 1</p>
+                </div>
+                <div className="mt-auto flex justify-end">
                   <Link
                     href=""
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -148,27 +140,23 @@ export default function BootcampPage() {
                 />
               </div>
             </div>
-            <div className="max-w-sm w-full md:mx-16 mx-14 my-3 h-[450px] border md:my-10  relative">
-              <Link href="">
-                <Image
-                  className="rounded-t-lg"
-                  src="/assets/bootcamp/frontend.png"
-                  alt=""
-                  width={500}
-                  height={300}
-                  layout="responsive"
-                />
-              </Link>
-              <div className="p-5">
-                <Link href="">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            <div className="max-w-sm bg-[#D8EFD3] w-full md:mx-8 mx-14 my-3 h-[450px] border md:my-10 relative flex flex-col">
+              <Image
+                className="rounded-t-lg"
+                src="/assets/bootcamp/frontend.png"
+                alt=""
+                width={500}
+                height={300}
+                layout="responsive"
+              />
+              <div className="p-5 -mt-20 flex flex-col flex-grow">
+                <div>
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight">
                     Test 1
                   </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-                  Description 1
-                </p>
-                <div className="flex justify-center">
+                  <p className="mb-3 font-normal">Description 1</p>
+                </div>
+                <div className="mt-auto flex justify-end">
                   <Link
                     href=""
                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -201,113 +189,6 @@ export default function BootcampPage() {
                 />
               </div>
             </div>
-            <div className="max-w-sm w-full md:mx-16 mx-14 my-3 h-[450px] border md:my-10  relative">
-              <Link href="">
-                <Image
-                  className="rounded-t-lg"
-                  src="/assets/bootcamp/frontend.png"
-                  alt=""
-                  width={500}
-                  height={300}
-                  layout="responsive"
-                />
-              </Link>
-              <div className="p-5">
-                <Link href="">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                    Test 1
-                  </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-                  Description 1
-                </p>
-                <div className="flex justify-center">
-                  <Link
-                    href=""
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Read more
-                    <svg
-                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </Link>
-                </div>
-              </div>
-              <div className="absolute md:top-[147px] top-[148px] left-0">
-                <Image
-                  src="/assets/bootcamp/rectangle.png"
-                  alt="Rectangle"
-                  width={400}
-                  height={200}
-                />
-              </div>
-            </div>
-            <div className="max-w-sm w-full md:mx-16 mx-14 my-3 h-[450px] border md:my-10  relative">
-              <Link href="">
-                <Image
-                  className="rounded-t-lg"
-                  src="/assets/bootcamp/frontend.png"
-                  alt=""
-                  width={500}
-                  height={300}
-                  layout="responsive"
-                />
-              </Link>
-              <div className="p-5">
-                <Link href="">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                    Test 1
-                  </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">
-                  Description 1
-                </p>
-                <div className="flex justify-center">
-                  <Link
-                    href=""
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Read more
-                    <svg
-                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </Link>
-                </div>
-              </div>
-              <div className="absolute md:top-[147px] top-[148px] left-0">
-                <Image
-                  src="/assets/bootcamp/rectangle.png"
-                  alt="Rectangle"
-                  width={400}
-                  height={200}
-                />
-              </div>
-            </div>
-           
           </div>
         </div>
       </section>

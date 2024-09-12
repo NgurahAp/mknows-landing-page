ed59d5ca7fbb18810f9d7b8f7bcf44803ca533c2
Arya <ngurahpratama2002@gmail.com>
2024-09-12 11:15:22 +0700

    adding container dropdown to navigation Bootcamp program

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           />
         </div>
         {!isMediumScreen && (
-          <form className="mx-auto pl-[60px] flex  justify-center h-[43px]">
+          <form className="mx-auto pl-[60px] flex justify-center h-[43px]">
             <div className="relative bg-[#F5F5F5] overflow-hidden w-[310px] rounded-[36px]">
               <div className="absolute inset-y-0 start-1 flex items-center ps-2 pointer-events-none">
                 <Image
@@ -96,6 +96,7 @@ const Navbar = () => {
           >
             Home
           </Link>
+          {/* Bootcamp Navbar desktop */}
           <div className="relative group">
             <Link
               href="/bootcamp"
@@ -103,7 +104,123 @@ const Navbar = () => {
               className="px-[20px] hover:text-blue-500 flex items-center"
             >
               Bootcamp Program
+              <Image
+                src="/assets/dropdown-icon.png"
+                alt="Dropdown Icon"
+                width={14}
+                height={14}
+                className="ml-2"
+              />
             </Link>
+            {/* Dropdown value Bootcamp */}
+            <div className="absolute hidden -ml-[58vw] group-hover:block left-0 w-[110vw] h-[50vh] bg-white shadow-lg pt-4">
+              <div className="container mx-auto px-4 py-10">
+                <hr className="border-t-4 border-[#C1FA31] my-4 w-full mx-auto rounded-full" />
+                <div className="flex ">
+                  {/* Column 1 - 1/4 width */}
+                  <div className="w-1/4 pr-4">
+                    <h3 className="font-bold mb-2">Column 1</h3>
+                    <ul className="space-y-2 -ml-2">
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 1
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 2
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 3
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+
+                  {/* Column 2 - 2/4 width */}
+                  <div className="w-2/4 px-4">
+                    <h3 className="font-bold mb-2">Column 2</h3>
+                    <ul className="space-y-2 -ml-2">
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 4
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 5
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 6
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+
+                  {/* Column 3 - 1/4 width */}
+                  <div className="w-1/4 pl-4">
+                    <h3 className="font-bold mb-2">Column 3</h3>
+                    <ul className="space-y-2 -ml-2">
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 7
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 8
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          href="#"
+                          onClick={handleLinkClick}
+                          className="block hover:bg-gray-100 p-2"
+                        >
+                          Bootcamp 9
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="relative group">
             <Link
@@ -249,111 +366,38 @@ const Navbar = () => {
             <Link
               href="/"
               onClick={handleLinkClick}
-              className="py-2 hover:text-blue-500"
+              className="py-2 text-lg hover:text-blue-500"
             >
               Home
             </Link>
-            <div className="py-2 text-center hover:text-blue-500 relative group">
-              <Link href="/bootcamp" onClick={handleLinkClick}>
-                Bootcamp Program
-              </Link>
-            </div>
-            <div className="py-2 text-center hover:text-blue-500 relative group">
+            <Link
+              href="#"
+              onClick={handleLinkClick}
+              className="py-2 text-lg hover:text-blue-500"
+            >
+              Bootcamp Program
+            </Link>
+            <Link
+              href="#"
+              onClick={handleLinkClick}
+              className="py-2 text-lg hover:text-blue-500"
+            >
               New Training
-              <ul className="hidden group-hover:block bg-white shadow-md mt-2">
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Training 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Training 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Training 3
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="py-2 text-center hover:text-blue-500 relative group">
+            </Link>
+            <Link
+              href="#"
+              onClick={handleLinkClick}
+              className="py-2 text-lg hover:text-blue-500"
+            >
               What We Do
-              <ul className="hidden group-hover:block bg-white shadow-md mt-2">
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Service 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Service 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Service 3
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="py-2 text-center hover:text-blue-500 relative group">
+            </Link>
+            <Link
+              href="#"
+              onClick={handleLinkClick}
+              className="py-2 text-lg hover:text-blue-500"
+            >
               More
-              <ul className="hidden group-hover:block bg-white shadow-md mt-2">
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Option 1
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Option 2
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="#"
-                    onClick={handleLinkClick}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    Option 3
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            </Link>
           </div>
         </div>
       )}

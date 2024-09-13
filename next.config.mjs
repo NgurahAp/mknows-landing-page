3bf314c0fbaf071ceee43f0e9a0d5dd871fe196a
Arya <ngurahpratama2002@gmail.com>
2024-09-13 21:24:09 +0700

    adding some code to fix the ui problem on the different device

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // tambahkan konfigurasi lain jika diperlukan
+};
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0,  shrink-to-fit=no"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </head>
       <body className={poppins.className}>

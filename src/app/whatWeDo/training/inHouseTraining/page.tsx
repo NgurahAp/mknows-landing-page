03470720e47468ac4dea9d152c29657d9fa81cbb
Arya <ngurahpratama2002@gmail.com>
2024-09-17 14:15:09 +0700

    adding in-house training page

--- src/app/whatWeDo/training/inHouseTraining/page.tsx
+++ src/app/whatWeDo/training/inHouseTraining/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 
 export default function Training() {
+  const images = Array.from(
+    { length: 10 },
+    (_, i) => `/assets/whatWeDo/inHouseRow1-1.png`
+  );
+
   return (
     <main>
       <section className="w-full pt-32 bg-white">
         <div className="container mx-auto px-48">
-          <h2 className="text-5xl font-bold text-center mb-8 py-9">
+          <h2 className="text-5xl text-[#225677] font-bold text-center mb-8 py-9">
             In-House Training
           </h2>
           <div className="flex flex-col md:flex-row items-center py-5">
@@ -56,29 +61,15 @@ export default function Training() {
             </div>
           </div>
           <div className="flex flex-col md:flex-row items-center py-5">
-            <div className="md:w-1/2 mb-6 md:mb-0 flex justify-center">
+            <div className="md:w-1/2 mb-6 md:mb-0 flex items-center justify-center">
               <Image
-                src="/assets/whatWeDo/inHouse1.png"
+                src="/assets/whatWeDo/inHouse3.png"
                 alt="Deskripsi gambar"
                 width={366}
                 height={375}
                 className="rounded-lg shadow-md "
               />
             </div>
-            <div className="md:w-1/2 ">
-              <p className="text-2xl leading-relaxed">
-                Pelatihan Internal merupakan program pelatihan yang
-                diselenggarakan oleh M-KNOWS CONSULTING bekerjasama dengan
-                Perusahaan atau Instansi pemerintah untuk meningkatkan skill
-                karyawan sehingga meningkatkan keunggulan kompetitif di bidang
-                Sumberdaya Manusia dengan materi tertentu, seperti Selling
-                Skills, Leadership, Executive Coaching & Business Mentoring,
-                Train The Trainer, Collection Skills, Credit Analyst dan
-                lain-lain sesuai kebutuhan perusahaan atau instansi.
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col md:flex-row items-center py-5">
             <div className="md:w-1/2 ">
               <p className="text-2xl leading-relaxed">
                 Sistem penyelenggaraan pelatihan dapat diadakan dalam kelas
@@ -90,15 +81,93 @@ export default function Training() {
                 luar Jabodetabek dan di seluruh wilayah Indonesia.
               </p>
             </div>
-            <div className="md:w-1/2 mb-6 md:mb-0 flex items-center justify-center">
-              <Image
-                src="/assets/whatWeDo/inHouse3.png"
-                alt="Deskripsi gambar"
-                width={366}
-                height={375}
-                className="rounded-lg shadow-md "
-              />
+          </div>
+          <h2 className="text-5xl text-[#225677] font-bold pt-20 mt-8 mb-4 ">
+            Materi pelatihan yang diselenggarakan:
+          </h2>
+          <div className="flex flex-col  md:flex-row items-center pt-6 ">
+            <div className="mb-6 md:mb-0">
+              <h2 className="text-5xl text-[#225677] font-bold mb-8">1.</h2>
             </div>
+            <div className="pl-7">
+              <p className="text-3xl leading-relaxed text-center md:text-left">
+                Tempat Penyelenggaraan Training: Pelatihan dapat diselenggarakan
+                di Perusahaan atau Instansi. Pelatihan dapat diselenggarakan di
+                tempat yang ditentukan misal: Hotel, Training Center perusahaan,
+                dll.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col  md:flex-row  pt-5">
+            <div className="mb-6 md:mb-0">
+              <h2 className="text-5xl text-[#225677] font-bold ">2.</h2>
+            </div>
+            <div className="pl-5">
+              <p className="text-3xl leading-relaxed md:text-left">
+                Fasilitas : Sertifikat,Training Kit,Hard copy
+                materi,Hotel/Resort
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col  md:flex-row items-center pt-5  ">
+            <div className="mb-6 md:mb-0">
+              <h2 className="text-5xl text-[#225677] font-bold mb-8">3.</h2>
+            </div>
+            <div className="pl-5">
+              <p className="text-3xl leading-relaxed text-center md:text-left">
+                Tempat Penyelenggaraan Training: Pelatihan dapat diselenggarakan
+                di Perusahaan atau Instansi. Pelatihan dapat diselenggarakan di
+                tempat yang ditentukan misal: Hotel, Training Center perusahaan,
+                dll.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto">
+          <h2 className="text-4xl text-[#225677] font-semibold text-center mb-8 py-9 pt-20">
+            Our Clients
+          </h2>
+          <div className="w-full h-24 overflow-x-auto flex space-x-12 p-3 my-5 scrollbar-hide">
+            {images.map((src, index) => (
+              <div key={index} className="flex-shrink-0 h-full">
+                <Image
+                  src={src}
+                  alt={`In-house activity ${index + 1}`}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="h-full w-auto object-contain"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="w-full h-28 overflow-x-auto flex space-x-12 p-3 my-5 scrollbar-hide">
+            {images.map((src, index) => (
+              <div key={index} className="flex-shrink-0 h-full">
+                <Image
+                  src="/assets/whatWeDo/inHouseRow2-1.png"
+                  alt={`In-house activity ${index + 1}`}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="h-full w-auto object-contain"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="w-full h-24 overflow-x-auto flex space-x-12 p-3 my-5  scrollbar-hide">
+            {images.map((src, index) => (
+              <div key={index} className="flex-shrink-0 h-full">
+                <Image
+                  src="/assets/whatWeDo/inHouseRow3-1.png"
+                  alt={`In-house activity ${index + 1}`}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="h-full w-auto object-contain"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>

29d1e2ea23750882b2043f1149d758a8a0e9add2
Arya <ngurahpratama2002@gmail.com>
2024-09-18 15:52:04 +0700

    feat: complete desktop layout for Hybrid Officer Development pag

--- src/app/whatWeDo/hybridOfficerDevelopment/page.tsx
+++ src/app/whatWeDo/hybridOfficerDevelopment/page.tsx
@@ -60,7 +60,7 @@ export default function Training() {
           </div>
         </div>
       </section>
-      <section className="flex flex-col items-center p-20  min-h-screen bg-gray-100">
+      <section className="flex flex-col items-center p-20  min-h-screen ">
         {/* Pengembangan program singkat hibrid */}
         <div className="text-center max-w-full mx-auto ">
           <h1 className="text-4xl text-[#648B04] font-semibold mb-8">
@@ -169,6 +169,134 @@ export default function Training() {
               />
             </div>
           </div>
+        </div>
+        {/* Proses Pelatihan */}
+        <div className=" w-full pt-28 ">
+          <h1 className="text-4xl text-[#648B04] font-semibold mb-10  text-center">
+            Proses Pelatihan di Kampus Gratis
+          </h1>
+
+          <div className="flex flex-col md:flex-row items-center py-7 px-36 ">
+            <div className="w-1/3 mb-6 md:mb-0 flex justify-center">
+              <Image
+                src="/assets/whatWeDo/hybridOfficer/proses-pelatihan.png"
+                alt="Deskripsi gambar"
+                width={317}
+                height={300}
+                className="rounded-lg shadow-md "
+              />
+            </div>
+            <div className="w-2/3 ">
+              <p className="text-2xl mb-10 ml-20 leading-relaxed">
+                Materi diberikan secara synchronous (online meeting) dan
+                asynchronous (video ajar dan white paper di kampusgratis.id).
+                Sesi tatap muka diperlukan untuk penugasan dan mentoring
+                langsung. Setiap materi dilengkapi kuis, latihan, penugasan,
+                ujian akhir, dan uji kompetensi (studi kasus, role play, dan
+                drill) untuk evaluasi standar kompetensi.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Monitoring & Evaluasi */}
+        <div className=" w-4/5 pt-20  ">
+          <h1 className="text-4xl text-[#648B04] font-semibold mb-10  text-center">
+            Monitoring & Evaluasi
+          </h1>
+          <h1 className="text-3xl text-center">
+            Peserta program akan dimonitor kinerjanya meliputi:
+          </h1>
+          <div
+            className="relative h-56 w-11/12 mt-12"
+            style={{
+              backgroundImage:
+                "url('/assets/whatWeDo/hybridOfficer/left-right-gradient.png')",
+            }}
+          >
+            <div className="container mx-auto h-full pl-36 flex items-center ">
+              <h2 className="text-5xl ">01</h2>
+              <div className="h-32 border-l-2 mx-14 border-gray-400 "></div>
+              <Image
+                src="/assets/whatWeDo/hybridOfficer/professional-working-icon.png"
+                alt="Professional Working Attitude"
+                width={47}
+                height={47}
+                className="mr-14"
+              />
+              <div>
+                <h1 className="text-3xl font-semibold mb-6">
+                  Professional Working Attitude
+                </h1>
+                <h2 className="text-3xl pr-5">
+                  Meliputi disiplin, intensi belajar, sopan santun, tingkat
+                  energi, Fokus dan komitment.
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div
+            className="relative h-56 w-full ml-20"
+            style={{
+              backgroundImage:
+                "url('/assets/whatWeDo/hybridOfficer/right-left-gradient.png')",
+            }}
+          >
+            <div className="container mx-auto h-full pl-36 flex items-center ">
+              <h2 className="text-5xl ">02</h2>
+              <div className="h-32 border-l-2 mx-14 border-gray-400 "></div>
+              <Image
+                src="/assets/whatWeDo/hybridOfficer/pantang-menyerah.png"
+                alt="Professional Working Attitude"
+                width={47}
+                height={47}
+                className="mr-14"
+              />
+              <div>
+                <h1 className="text-3xl font-semibold mb-6">
+                  Pantang Menyerah Terhadap Hasil{" "}
+                </h1>
+                <h2 className="text-3xl max-w-2xl">
+                  Meliputi kualitas hasil penugasan, nilai kinerja, nilai uji
+                  kompetensi.
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div
+            className="relative h-56 w-11/12 "
+            style={{
+              backgroundImage:
+                "url('/assets/whatWeDo/hybridOfficer/left-right-gradient.png')",
+            }}
+          >
+            <div className="container mx-auto h-full pl-36 flex items-center ">
+              <h2 className="text-5xl ">03</h2>
+              <div className="h-32 border-l-2 mx-14 border-gray-400 "></div>
+              <Image
+                src="/assets/whatWeDo/hybridOfficer/penguasaan-kompetensi-icon.png"
+                alt="Professional Working Attitude"
+                width={47}
+                height={47}
+                className="mr-14"
+              />
+              <div>
+                <h1 className="text-3xl font-semibold mb-6">
+                  Penguasaan Kompetensi Teknis & Non Teknis{" "}
+                </h1>
+                <h2 className="text-3xl pr-5">
+                  Meliputi standar kompetensi yang M-Knows Terapkan, dimana Gap
+                  Kompetensi akan di identifikasi dan ditentukan solusi
+                  perbaikannya.
+                </h2>
+              </div>
+            </div>
+          </div>
+          <h1 className="text-3xl pt-14">
+            Selama melakukan magang kerja, mahasiswa akan di monitoring dan
+            evaluasi Progres kerjanya oleh Kampus Gratis. Laporan yang dibuat
+            akan di berikan akses kepada mahasiswa dan pihak perusahaan, sesuai
+            kebutuhan dan kepentingannya.
+          </h1>
         </div>
       </section>
     </main>

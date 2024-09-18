576eccb005999b4e743bb92ce8df4fc820286f06
Arya <ngurahpratama2002@gmail.com>
2024-09-18 09:50:21 +0700

    done new navbar for desktop

--- src/components/UpdateNavbar.tsx
+++ src/components/UpdateNavbar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { BootcampCategory, bootcampData } from "./bootcampNavbarData";
 
 const UpdateNavbar: React.FC = () => {
   const [activeSection, setActiveSection] = useState<string | null>(null);
@@ -127,35 +128,250 @@ const UpdateNavbar: React.FC = () => {
 
       {/* Section yang ditampilkan setelah klik navbar */}
       {activeSection === "bootcampProgram" && (
-        <section className="bg-green-100 w-full h-[50vh] flex items-center justify-center fixed top-24 left-0 z-50">
-          <h2 className="text-3xl font-bold">bootcamp Us Section</h2>
-          <p className="mt-4">Here is more information bootcamp us.</p>
+        <section className="bg-white w-full h-auto flex items-center justify-center fixed top-24 left-0 z-50">
+          <div className="container mx-auto px-4 py-10">
+            <div className="flex items-center my-4 w-full mx-auto">
+              <Image
+                src="/assets/home/navbarLine.png"
+                alt="Dropdown Icon"
+                layout="responsive"
+                width={100}
+                height={14}
+                className="w-full h-auto -ml-2"
+              />
+            </div>
+            <div className="w-full overflow-x-auto">
+              <div className="flex pb-4">
+                {bootcampData.map(
+                  (category: BootcampCategory, index: number) => (
+                    <div
+                      className="flex-shrink-0 w-auto max-w-2/4 mx-10"
+                      key={index}
+                    >
+                      {/* Lebar otomatis dengan batas maksimal 50% */}
+                      <h3 className="font-semibold text-lg mb-2 underline">
+                        {category.category}
+                      </h3>
+                      <ul className="space-y-2">
+                        {category.items.map((item: string, idx: number) => (
+                          <li key={idx}>
+                            <Link
+                              href="#"
+                              // onClick={handleLinkClick}
+                              className="block hover:bg-gray-100 py-2 text-lg"
+                            >
+                              {item}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+            <div className="mt-10">
+              <Link
+                href="/bootcamp"
+                onClick={() => handleClick("bootcampProgram")}
+              >
+                <button className="bg-[#D7E9F4] text-xl text-black font-semibold py-2 px-4 rounded-lg w-full">
+                  Lihat Selengkapnya
+                </button>
+              </Link>
+            </div>
+          </div>
         </section>
       )}
 
       {activeSection === "newTraining" && (
-        <section className="bg-yellow-100 w-full h-[50vh] flex items-center justify-center fixed top-24 left-0 z-50">
-          <h2 className="text-3xl font-bold">New Training Section</h2>
-          <p className="mt-4">
-            We offer a variety of services to meet your needs.
-          </p>
+        <section className="bg-white w-full h-auto flex items-center justify-center fixed top-24 left-0 z-50">
+          <div className="container mx-auto px-4 py-10">
+            <div className="flex">
+              {/* Column 1 - 1/4 width */}
+              <div className="w-full pr-4">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="#"
+                      // onClick={handleLinkClick}
+                      className="block hover:bg-gray-100 py-2 text-lg "
+                    >
+                      AI Sales Tools
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      // onClick={handleLinkClick}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Virtual Reality
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
       )}
 
       {activeSection === "whatWeDo" && (
-        <section className="bg-red-100 w-full h-[50vh] flex items-center justify-center fixed top-24 left-0 z-50">
-          <h2 className="text-3xl font-bold">whatWeDo Us Section</h2>
-          <p className="mt-4">
-            Get in touch with us through the following ways.
-          </p>
+        <section className="bg-white w-full h-auto flex items-center justify-center fixed top-24 left-0 z-50">
+          <div className="container mx-auto px-4 py-10">
+            <div className="flex justify-center ">
+              {/* Column 1 - 1/4 width */}
+              <div className="w-1/3 ">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="/whatWeDo/training"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg "
+                    >
+                      Public, In-House Training, Online Learning
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Consulting Service{" "}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Asessment for Technical Competency
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              {/* Column 2 - 2/4 width */}
+              <div className="w-1/3 ">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Learning Management System
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Consulting Service
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Outbound & Gathering
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              {/* Column 3 - 1/4 width */}
+              <div className="w-1/3 ">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("whatWeDo")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Executive Coaching & Bussines Mentoring
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
       )}
       {activeSection === "more" && (
-        <section className="bg-red-100 w-full h-[50vh] flex items-center justify-center fixed top-24 left-0 z-50">
-          <h2 className="text-3xl font-bold">More Us Section</h2>
-          <p className="mt-4">
-            Get in touch with us through the following ways.
-          </p>
+        <section className="bg-white w-full h-auto flex items-center justify-center fixed top-24 left-0 z-50">
+          <div className="container mx-auto px-4 py-10">
+            <div className="flex  ">
+              {/* Column 1 - 1/4 width */}
+              <div className="w-1/3 ">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg "
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      History
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Vision & Mission
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              {/* Column 2 - 2/4 width */}
+              <div className="w-1/3 ">
+                <ul className="space-y-2 -ml-2">
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Our Clients
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Our Facilitator
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="#"
+                      onClick={() => handleClick("more")}
+                      className="block hover:bg-gray-100 py-2 text-lg"
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
       )}
     </div>

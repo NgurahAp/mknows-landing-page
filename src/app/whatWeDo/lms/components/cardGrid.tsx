81f9c933d29afc639b930e0fef676acff05c6b5e
Arya <ngurahpratama2002@gmail.com>
2024-09-19 13:58:20 +0700

    fixin the card on LMS page

--- src/app/whatWeDo/lms/components/cardGrid.tsx
+++ src/app/whatWeDo/lms/components/cardGrid.tsx
@@ -8,38 +8,31 @@ interface Card {
 
 interface CardGridProps {
   cards: Card[]; // cards adalah array dari Card
-  gridCols: number; // gridCols adalah number
 }
 
-const CardGrid: React.FC<CardGridProps> = ({ cards, gridCols }) => {
+const CardGrid: React.FC<CardGridProps> = ({ cards,  }) => {
   return (
-    <div className="flex justify-center items-center py-20 ">
-      <div
-        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-${gridCols} gap-20`}
-      >
-        {cards.map((card: Card, index: number) => (
-          <div
-            key={index}
-            className="bg-white shadow-lg w-80 rounded-2xl p-6 text-center"
-          >
-            <div className="flex justify-center mb-6">
-              <Image
-                src={card.icon}
-                alt={card.title}
-                width={40}
-                height={40}
-                className="mx-auto"
-              />
-            </div>
-            <h2 className="text-xl font-semibold text-[#648B04] text-left mb-2">
-              {card.title}
-            </h2>
-            <p className="text-gray-600 text-xl text-left">
-              {card.description}
-            </p>
+    <div className={`flex flex-wrap justify-center py-20 gap-12`}>
+      {cards.map((card: Card, index: number) => (
+        <div
+          key={index}
+          className="bg-white shadow-lg w-80 rounded-2xl p-6 text-center"
+        >
+          <div className="flex justify-center mb-6">
+            <Image
+              src={card.icon}
+              alt={card.title}
+              width={40}
+              height={40}
+              className="mx-auto"
+            />
           </div>
-        ))}
-      </div>
+          <h2 className="text-xl font-semibold text-[#648B04] text-left mb-2">
+            {card.title}
+          </h2>
+          <p className="text-gray-600 text-xl text-left">{card.description}</p>
+        </div>
+      ))}
     </div>
   );
 };

76a8b2c77a363ed798787f6d2c25c828cade887c
zahrahatiqah <zahrahatiqah1207@gmail.com>
2024-09-24 12:51:11 +0700

    responsif online learning page

--- src/app/whatWeDo/training/onlineTraining/page.tsx
+++ src/app/whatWeDo/training/onlineTraining/page.tsx
@@ -5,22 +5,24 @@ import Link from "next/link";
 export default function NewTrainingPage() {
   return (
     <main>
-      <section className="flex flex-col md:py-20 py-10 md:px-28 px-10 ">
-        <div className="text-center pb-10 mt-[133px]">
-          <h2 className="text-[32px] leading-[38.4px] font-semibold mb-2">
+      <section className="flex flex-col py-10 md:py-20 px-6 md:px-28">
+        <div className="text-center pb-6 mt-20 md:mt-[133px]">
+          <h2 className="text-[24px] md:text-[32px] leading-[28.8px] md:leading-[38.4px] font-semibold mb-2">
             Public Webtraining
           </h2>
-          <h2 className="text-[32px] leading-[38.4px] font-semibold mb-4 text-[#2D729F]">
+          <h2 className="text-[24px] md:text-[32px] leading-[28.8px] md:leading-[38.4px] font-semibold mb-4 text-[#2D729F]">
             2024
           </h2>
         </div>
-        <Link href="/target-page" className="w-[450px]">
-          <div className="bg-slate-100 text-[#2D729F] border border-[#388FC7] py-3 px-2 rounded-lg font-medium text-center hover:bg-gray-200">
+
+        <Link href="/whatWeDo/training/onlineTraining/formulir" className="sm:w-[370px] md:w-[450px]">
+          <div className="w-full md:w-auto sm:w-auto bg-slate-100 text-[#2D729F] border border-[#388FC7] py-3 px-4 text-sm md:text-base rounded-lg font-medium text-center hover:bg-gray-200">
             Download Jadwal Public Training Tahun 2024
           </div>
         </Link>
-        <div className="container mx-auto">
-          <div className="flex flex-wrap justify-center gap-10">
+
+        <div className="container mx-auto mt-10">
+          <div className="flex flex-wrap justify-center gap-6 md:gap-10">
             {onlineLearningData?.map((training) => (
               <OnlineLearningSection key={training?.id} training={training} />
             ))}

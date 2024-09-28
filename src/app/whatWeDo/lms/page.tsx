8a7e1b8231c69d6e632ed6c048a6f6d6b0d7b745
Arya <ngurahpratama2002@gmail.com>
2024-09-28 19:19:04 +0700

    done LMS responsive page

--- src/app/whatWeDo/lms/page.tsx
+++ src/app/whatWeDo/lms/page.tsx
@@ -12,34 +12,31 @@ export default function LMS() {
       >
         <div className="w-2/3">
           <div className="md:pl-20 pl-5  flex flex-col justify-center">
-            <h1 className="font-semibold mb-4 text-xl text-[#388FC7] md:text-5xl leading-[50px] text-left">
-              Learning Management <br />
-            </h1>
-            <h1 className="font-semibold  text-xl text-[#388FC7] md:text-5xl leading-[50px] text-left">
-              System
+            <h1 className="font-semibold mb-4 text-xl text-[#388FC7] md:text-5xl md:leading-10 leading-6 text-left">
+              Learning Management System
               <span className="text-black "> (LMS)</span>
             </h1>
           </div>
         </div>
       </section>
       <section className="flex flex-col  pt-20 mx-auto w-10/12  min-h-screen ">
-        <h1 className="text-5xl text-center  font-medium mb-8">
+        <h1 className="md:text-5xl text-2xl text-center  font-medium mb-8">
           Pengembangan LMS Berbasis Moodle
         </h1>
-        <p className="text-3xl text-center pb-28 mx-36">
+        <p className="md:text-3xl text-lg text-center md:pb-28 pb-10 md:mx-36">
           Kami mengembangkan Learning Management System (LMS) menggunakan Moodle
           dengan fitur fleksibel untuk kegiatan belajar, pengaturan konten
           multimedia, dan dukungan untuk interaksi belajar yang beragam. Desain
           LMS dapat disesuaikan, terintegrasi dengan SCORM, dan kompatibel
           dengan perangkat mobile.
         </p>
-        <h1 className="text-5xl text-[#648B04] text-center font-medium mb-10">
+        <h1 className="md:text-5xl text-3xl text-[#648B04] text-center font-medium mb-10">
           Konten Pembelajaran Digital
         </h1>
-        <p className="text-4xl flex items-start justify-start text-left text-[#648B04] font-medium mb-5">
+        <p className="md:text-4xl text-2xl flex items-start justify-start text-left text-[#648B04] font-medium mb-5">
           Modul Interaktif
         </p>
-        <p className="text-3xl  pb-10 ">
+        <p className="md:text-3xl text-xl  pb-10 ">
           Modul Interaktif Modul pembelajaran yang kami kembangkan mengacu pada
           konsep Activity Based Learning dengan memperhatikan level interaktif
           dari setiap informasi yang perlu disampaikan. <br /> <br /> Penggunaan
@@ -48,10 +45,10 @@ export default function LMS() {
           still atau bergerak tergantung pada kebutuhan instruksional Anda.
         </p>
         <CardGrid cards={levelCards} />
-        <p className="text-4xl flex items-start justify-start text-left text-[#22776C] font-medium mb-5">
+        <p className="md:text-4xl text-2xl flex items-start justify-start text-left text-[#22776C] font-medium mb-5">
           Vidio Instriksional
         </p>
-        <p className="text-3xl  pb-10 ">
+        <p className="md:text-3xl  text-xl pb-10 ">
           Video pembelajaran merupakan alternatif yang efektif untuk
           meningkatkan kualitas kegiatan belajar. Kami menyediakan berbagai
           jenis video sesuai kebutuhan instruksional Anda. <br /> <br />{" "}

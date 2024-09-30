4cf3b7472061e7d66f4ff5d3ecc0d3179f2d7c11
Arya <ngurahpratama2002@gmail.com>
2024-09-30 10:52:12 +0700

    rafactoring bootcamp navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { BootcampCategory, bootcampData } from "./bootcampNavbarData";
 import MobileNavbar from "./navbar/mobile";
 import { ChevronDown } from "lucide-react";
+import BootcampSection from "./navbar/bootcampSection";
 
 
 const Navbar: React.FC = () => {
@@ -173,59 +173,7 @@ const Navbar: React.FC = () => {
 
       {/* Section yang ditampilkan setelah klik navbar */}
       {activeSection === "bootcampProgram" && (
-        <section className="bg-white w-full h-auto flex items-center justify-center fixed top-24 left-0 z-50">
-          <div className="container mx-auto px-4 py-10">
-            <div className="flex items-center my-4 w-full mx-auto">
-              <Image
-                src="/assets/home/navbarLine.png"
-                alt="Dropdown Icon"
-                layout="responsive"
-                width={100}
-                height={14}
-                className="w-full h-auto -ml-2"
-              />
-            </div>
-            <div className="w-full overflow-x-auto">
-              <div className="flex pb-4">
-                {bootcampData.map(
-                  (category: BootcampCategory, index: number) => (
-                    <div
-                      className="flex-shrink-0 w-auto max-w-2/4 mx-10"
-                      key={index}
-                    >
-                      <h3 className="font-semibold text-lg mb-2 underline">
-                        {category.category}
-                      </h3>
-                      <ul className="space-y-2">
-                        {category.items.map((item: string, idx: number) => (
-                          <li key={idx}>
-                            <Link
-                              href="#"
-                              // onClick={handleLinkClick}
-                              className="block hover:text-blue-500 py-2 text-lg" // Mengganti hover background color menjadi biru
-                            >
-                              {item}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )
-                )}
-              </div>
-            </div>
-            <div className="mt-10">
-              <Link
-                href="/bootcamp"
-                onClick={() => handleClick("bootcampProgram")}
-              >
-                <button className="bg-[#D7E9F4] text-xl text-black font-semibold py-2 px-4 rounded-lg w-full">
-                  Lihat Selengkapnya
-                </button>
-              </Link>
-            </div>
-          </div>
-        </section>
+        <BootcampSection handleClick={handleClick} />
       )}
 
       {activeSection === "newTraining" && (
